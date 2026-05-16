1f3b4823547c085c4ad4ed86683a8ce9c27e1029
--Darkness-- <patrickcortez2024@gmail.com>
2026-05-16 19:32:41 +0800

    updated contexts: 50-60%

--- src/core/ContextRead.cs
+++ src/core/ContextRead.cs
@@ -5,6 +5,11 @@ internal class ContextReader
     readonly string jsonfile = Path.Combine(AppContext.BaseDirectory,"Contexts.json");
    public ContextReader()
     {
-            
+        if (!File.Exists(jsonfile))
+        {
+            File.Create(jsonfile);
+        }
+
+
     }
 }

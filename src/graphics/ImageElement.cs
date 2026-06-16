5b696ecdd42de88c18e69b5f0253a27ee0f4f0db
--Darkness-- <patrickcortez2024@gmail.com>
2026-06-16 14:50:10 +0800

    Fix ImageElement drawing nothing if width/height are not set

--- src/graphics/ImageElement.cs
+++ src/graphics/ImageElement.cs
@@ -2,7 +2,17 @@ namespace AurShell.Graphics;
 
 public class ImageElement : UIElement
 {
-    public VirtualScreen Image { get; set; }
+    private VirtualScreen _image;
+    public VirtualScreen Image 
+    { 
+        get => _image; 
+        set 
+        { 
+            _image = value; 
+            if (Width <= 0 && value != null) Width = value.Width;
+            if (Height <= 0 && value != null) Height = value.Height;
+        } 
+    }
 
     public override void Render(GraphicsContext g)
     {

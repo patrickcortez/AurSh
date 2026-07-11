874bfdfaa7953d65a2b2db57556fdc52542af48a
--Darkness-- <patrickcortez2024@gmail.com>
2026-07-12 07:16:39 +0800

    moved a few files

--- tests/obj/Debug/net8.0/TokenPrinter.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/TokenPrinter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TokenPrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d285467ecbdc1892f425604efad0cf8ab9bb443a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0044af9ff81bcabcc31726cc987c0319fcccb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TokenPrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TokenPrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
